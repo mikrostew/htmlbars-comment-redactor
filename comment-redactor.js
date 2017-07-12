f679268f50697ce720757f08fccdc70d3e714a1f
Michael Stewart <mikrostew@gmail.com>
2017-07-12 15:05:59 -0700

    support for ember-1.13

--- comment-redactor.js
+++ comment-redactor.js
@@ -3,7 +3,17 @@
 
 class CommentRedactor {
   transform(ast) {
-    this.syntax.traverse(ast, {
+    let traverse;
+
+    if (this.syntax && this.syntax.traverse) {
+      // glimmer
+      traverse = this.syntax.traverse;
+    }
+    else {
+      traverse = require('htmlbars/dist/cjs/htmlbars-syntax').traverse;
+    }
+
+    traverse(ast, {
       CommentStatement: {
         enter(node) {
           // overwrite whatever is in the comment
