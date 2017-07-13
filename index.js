713cc497e5fe5ceeb94e012cd51fcd405ecfa73d
Michael Stewart <mikrostew@gmail.com>
2017-07-12 17:10:27 -0700

    convert to use paralle API in ember-cli-htmlbars-inline-precompile

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ module.exports = {
   name: 'htmlbars-comment-redactor',
 
   setupPreprocessorRegistry(type, registry) {
-    registry.add('htmlbars-ast-plugin', {
+    const pluginInfo = this.buildPluginInfo();
+    // for parallel transpile - if this isn't known, it can be ignored and the plugin will work as usual
+    pluginInfo.parallelBabel = {
+      requireFile: __dirname,
+      buildUsing: 'buildPluginInfo',
+      params: {}
+    };
+
+    registry.add('htmlbars-ast-plugin', pluginInfo);
+  },
+
+  buildPluginInfo() {
+    return {
       name: 'htmlbars-comment-redactor',
       plugin: CommentRedactor,
       baseDir: () => __dirname
-    });
+    };
   }
 };
